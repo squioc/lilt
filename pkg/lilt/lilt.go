7f1b4667d758d70b245d6fe77c5c4b2a67534abf
Quioc Sébastien <sebastien.quioc@yahoo.com>
2016-01-22 21:02:05 +0100

    feat(lilt): Add lilt struct

---
+++ pkg/lilt/lilt.go
@@ -0,0 +1,37 @@
+package lilt
+
+import (
+	"bufio"
+	"io"
+	"time"
+)
+
+// Lilt provides copy bytes from the input to the output according to a period
+type Lilt struct {
+	input  *bufio.Scanner
+	output io.Writer
+	pulse  *time.Ticker
+}
+
+// NewLilt creates a new lilt
+func NewLilt(input *bufio.Scanner, output io.Writer, pulse *time.Ticker) *Lilt {
+	return &Lilt{input: input, output: output, pulse: pulse}
+}
+
+// NewLiltWithReader creates a new lilt with a reader as input
+func NewLiltWithReader(reader io.Reader, output io.Writer, pulse *time.Ticker) *Lilt {
+	return &Lilt{input: bufio.NewScanner(reader), output: output, pulse: pulse}
+}
+
+// Run copies bytes from the input to the output
+func (l *Lilt) Run() error {
+	for {
+		<-l.pulse.C
+		if !l.input.Scan() {
+			return nil
+		}
+		l.output.Write(l.input.Bytes())
+		l.output.Write([]byte{0x0A})
+	}
+	return nil
+}
